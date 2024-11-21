ebfaeb97e90092212b1cd42864d173966bdb95b5
Nicolás Snider <nicolas.snider@outlook.com>
2024-11-21 18:02:30 -0300

    reformat project

---
+++ src/Modules/Ordering/OrderingModule.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering;
+public static class OrderingModule
+{
+    public static IServiceCollection AddOrderingModule(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        /*services
+            .AddApplicationServices()
+            .AddInfrastructureServices(configuration)
+            .AddApiServices(configuration);*/
+
+
+        return services;
+
+    }
+
+    public static IApplicationBuilder UseOrderingModule(this IApplicationBuilder app)
+    {
+        return app;
+    }
+}
