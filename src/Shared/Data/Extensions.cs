b875493d2a60dd4272964bf7cc273b1d080e3916
Nicolás Snider <nicolas.snider@outlook.com>
2024-11-23 14:11:47 -0300

    Shared Seeder and Extensions

--- src/Shared/Data/Extensions.cs
+++ src/Shared/Data/Extensions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Shared.Data.Seed;
 
 namespace Shared.Data;
 public static class Extensions
@@ -10,7 +11,20 @@ public static class Extensions
     {
         MigrateDatabaseAsync<TContext>(app.ApplicationServices).GetAwaiter().GetResult();
 
+
+        SeedDataAsync(app.ApplicationServices).GetAwaiter().GetResult();
+
         return app;
+    }
+
+    private static async Task SeedDataAsync(IServiceProvider serviceProvider)
+    {
+        using var scope = serviceProvider.CreateScope();
+        var seeders = scope.ServiceProvider.GetServices<IDataSeeder>();
+        foreach (var seeder in seeders)
+        {
+            await seeder.SeedAllAsync();
+        }
     }
 
     private static async Task MigrateDatabaseAsync<TContext>(IServiceProvider serviceProvider)
