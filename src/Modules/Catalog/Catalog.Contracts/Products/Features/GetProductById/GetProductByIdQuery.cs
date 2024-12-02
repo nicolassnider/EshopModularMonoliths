3afd0664d11f374c3c318bb3a5cb02f80ef8325c
Nicolás Snider <nicolas.snider@outlook.com>
2024-12-02 17:48:17 -0300

    updating structure

--- src/Modules/Catalog/Catalog.Contracts/Products/Features/GetProductById/GetProductByIdQuery.cs
+++ src/Modules/Catalog/Catalog.Contracts/Products/Features/GetProductById/GetProductByIdQuery.cs
@@ -1,5 +1,5 @@
 ﻿namespace Catalog.Contracts.Products.Features.GetProductById;
+
 public record GetProductByIdQuery(Guid Id)
     : IQuery<GetProductByIdResult>;
-
 public record GetProductByIdResult(ProductDto Product);
--- src/Modules/Catalog/Catalog/Products/Features/GetProductById/GetProductByIdHandler.cs
+++ src/Modules/Catalog/Catalog/Products/Features/GetProductById/GetProductByIdHandler.cs
@@ -3,13 +3,23 @@
 internal class GetProductByIdHandler(CatalogDbContext dbContext)
     : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
 {
-    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
     {
+        // get products by id using dbContext
+        // return result
+
         var product = await dbContext.Products
-            .AsNoTracking()
-            .SingleOrDefaultAsync(p => p.Id == request.Id, cancellationToken);
-        if (product == null) throw new ProductNotFoundException(request.Id);
+                        .AsNoTracking()
+                        .SingleOrDefaultAsync(p => p.Id == query.Id, cancellationToken);
+
+        if (product is null)
+        {
+            throw new ProductNotFoundException(query.Id);
+        }
+
+        //mapping product entity to productdto
         var productDto = product.Adapt<ProductDto>();
+
         return new GetProductByIdResult(productDto);
     }
 }
