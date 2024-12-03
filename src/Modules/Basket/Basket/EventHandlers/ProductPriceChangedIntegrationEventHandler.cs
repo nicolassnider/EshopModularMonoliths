74022160f99be5721aec54405f3c1395a8a31914
Nicolás Snider <nicolas.snider@outlook.com>
2024-12-03 16:26:12 -0300

    update events

--- src/Modules/Basket/Basket/EventHandlers/ProductPriceChangedIntegrationEventHandler.cs
+++ src/Modules/Basket/Basket/EventHandlers/ProductPriceChangedIntegrationEventHandler.cs
@@ -1,16 +1,26 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Basket.Basket.Features.UpdateItemPriceInBasket;
+using Microsoft.Extensions.Logging;
 
 namespace Basket.Basket.EventHandlers;
 public class ProductPriceChangedIntegrationEventHandler
-    (ILogger<ProductPriceChangedIntegrationEventHandler> logger)
+    (
+    ISender sender,
+    ILogger<ProductPriceChangedIntegrationEventHandler> logger)
     : IConsumer<ProductPriceChangedIntegrationEvent>
 {
-    public Task Consume(ConsumeContext<ProductPriceChangedIntegrationEvent> context)
+    public async Task Consume(ConsumeContext<ProductPriceChangedIntegrationEvent> context)
     {
-        logger.LogInformation("Integration Event Handled: {IntegrationEvent}", context.Message.GetType().Name);
-        // find basket item 
+        logger.LogInformation("Integration Event handled: {IntegrationEvent}", context.Message.GetType().Name);
 
-        // mediatr new command and handler to find and update prices
-        return Task.CompletedTask;
+        // mediatr new command and handler to find products on basket and update price
+        var command = new UpdateItemPriceInBasketCommand(context.Message.ProductId, context.Message.Price);
+        var result = await sender.Send(command);
+
+        if (!result.IsSuccess)
+        {
+            logger.LogError("Error updating price in basket for product id: {ProductId}", context.Message.ProductId);
+        }
+
+        logger.LogInformation("Price for product id: {ProductId} updated in basket", context.Message.ProductId);
     }
 }
--- src/Modules/Catalog/Catalog/Products/EventHandlers/ProductPriceChangedEventHandler.cs
+++ src/Modules/Catalog/Catalog/Products/EventHandlers/ProductPriceChangedEventHandler.cs
@@ -6,9 +6,9 @@ public class ProductPriceChangedEventHandler(
 {
     public async Task Handle(ProductPriceChangedEvent notification, CancellationToken cancellationToken)
     {
-        logger.LogInformation("Domain Event handlerd: {DomainEvent}", notification.GetType().Name);
+        logger.LogInformation("Domain Event handled: {DomainEvent}", notification.GetType().Name);
 
-
+        // Publish product price changed integration event for update basket prices
         var integrationEvent = new ProductPriceChangedIntegrationEvent
         {
             ProductId = notification.Product.Id,
@@ -16,7 +16,7 @@ public class ProductPriceChangedEventHandler(
             Category = notification.Product.Category,
             Description = notification.Product.Description,
             ImageFile = notification.Product.ImageFile,
-            Price = notification.Product.Price
+            Price = notification.Product.Price //set updated product price
         };
 
         await bus.Publish(integrationEvent, cancellationToken);
