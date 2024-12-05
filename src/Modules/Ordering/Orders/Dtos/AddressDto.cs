d2685d643a17608745b86b7a9dcce40f053c4c50
Nicolás Snider <nicolas.snider@outlook.com>
2024-12-05 17:39:38 -0300

    dtos

---
+++ src/Modules/Ordering/Orders/Dtos/AddressDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Ordering.Orders.Dtos;
+public record class AddressDto
+    (
+    string Street,
+    string City,
+    string State,
+    string Country,
+    string ZipCode);
---
+++ src/Modules/Ordering/Orders/Dtos/OrderDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordering.Orders.Dtos;
+public record OrderDto(
+    Guid Id,
+    Guid CustomerId,
+    string orderName,
+    AddressDto ShhippingAddress,
+    AddressDto BillingAddress,
+    PaymentDto Payment,
+    List<OrderItemDto> Items
+    );
