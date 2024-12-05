d9c5ca685cf39149a412fa25ea9bb4b17f361311
Nicolás Snider <nicolas.snider@outlook.com>
2024-12-05 18:52:14 -0300

    adds order endpoins

--- src/Modules/Ordering/GlobalUsings.cs
+++ src/Modules/Ordering/GlobalUsings.cs
@@ -1,7 +1,10 @@
-﻿global using FluentValidation;
+﻿global using Carter;
+global using FluentValidation;
 global using Mapster;
 global using MediatR;
 global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Routing;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Diagnostics;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
