5823e3133909ae843b7ee598e5d9c0bcc97c21e9
Nicolás Snider <nicolas.snider@outlook.com>
2024-12-05 18:29:35 -0300

    create feature handlers

---
+++ src/Modules/Ordering/Orders/Features/DeleteOrder/DeleteOrderHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace Ordering.Orders.Features.DeleteOrder;
+
+public record DeleteOrderCommand(Guid OrderId)
+    : ICommand<DeleteOrderResult>;
+
+public record DeleteOrderResult(bool IsSuccess);
+
+internal class DeleteOrderHandler(OrderingDbContext dbContext)
+    : ICommandHandler<DeleteOrderCommand, DeleteOrderResult>
+{
+    public async Task<DeleteOrderResult> Handle(DeleteOrderCommand command, CancellationToken cancellationToken)
+    {
+        var order = await dbContext.Orders
+            .FindAsync([command.OrderId], cancellationToken);
+
+        if (order is null) throw new OrderNotFoundException(command.OrderId);
+
+        dbContext.Orders.Remove(order);
+        await dbContext.SaveChangesAsync(cancellationToken);
+        return new DeleteOrderResult(true);
+    }
+}
