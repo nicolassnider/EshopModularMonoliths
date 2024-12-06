07782035514c2ef031abd2381dccaaa9982acbcd
Nicolás Snider <nicolas.snider@outlook.com>
2024-12-06 17:10:52 -0300

    api formatting

--- src/Bootstrapper/Api/Program.cs
+++ src/Bootstrapper/Api/Program.cs
@@ -1,8 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Host
-    .UseSerilog((context, config) =>
-    config.ReadFrom.Configuration(context.Configuration));
+builder.Host.UseSerilog((context, config) => config.ReadFrom.Configuration(context.Configuration));
 
 // Add services to the container.
 
@@ -11,34 +9,31 @@ var catalogAssembly = typeof(CatalogModule).Assembly;
 var basketAssembly = typeof(BasketModule).Assembly;
 var orderingAssembly = typeof(OrderingModule).Assembly;
 
-builder.Services
-    .AddCarterWithAssemblies(catalogAssembly, basketAssembly, orderingAssembly);
+builder.Services.AddCarterWithAssemblies(catalogAssembly, basketAssembly, orderingAssembly);
 
-builder.Services
-    .AddMediatRWithAssemblies(catalogAssembly, basketAssembly, orderingAssembly);
+builder.Services.AddMediatRWithAssemblies(catalogAssembly, basketAssembly, orderingAssembly);
 
-builder.Services
-    .AddStackExchangeRedisCache(options =>
+builder.Services.AddStackExchangeRedisCache(options =>
 {
     options.Configuration = builder.Configuration.GetConnectionString("Redis");
 });
 
-builder.Services
-    .AddMassTransitWithAssemblies(builder.Configuration, catalogAssembly, basketAssembly);
+builder.Services.AddMassTransitWithAssemblies(
+    builder.Configuration,
+    catalogAssembly,
+    basketAssembly
+);
 
-builder.Services
-    .AddKeycloakWebApiAuthentication(builder.Configuration);
-builder.Services
-    .AddAuthorization();
+builder.Services.AddKeycloakWebApiAuthentication(builder.Configuration);
+builder.Services.AddAuthorization();
 
 //module services: catalog, basket, ordering
-builder.Services
-    .AddCatalogModule(builder.Configuration)
+builder
+    .Services.AddCatalogModule(builder.Configuration)
     .AddBasketModule(builder.Configuration)
     .AddOrderingModule(builder.Configuration);
 
-builder.Services
-    .AddExceptionHandler<CustomExceptionHandler>();
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
 
@@ -50,9 +45,6 @@ app.UseExceptionHandler(options => { });
 app.UseAuthentication();
 app.UseAuthorization();
 
-app
-    .UseCatalogModule()
-    .UseBasketModule()
-    .UseOrderingModule();
+app.UseCatalogModule().UseBasketModule().UseOrderingModule();
 
 app.Run();
