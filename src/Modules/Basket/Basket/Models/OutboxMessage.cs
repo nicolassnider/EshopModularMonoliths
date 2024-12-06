d968010625db28b762a12e208f07731625d027f1
Nicolás Snider <nicolas.snider@outlook.com>
2024-12-06 15:46:23 -0300

    nre outboxMessage model

---
+++ src/Modules/Basket/Basket/Models/OutboxMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace Basket.Basket.Models;
+public class OutboxMessage : Entity<Guid>
+{
+    public string Type { get; set; } = default!;
+    public string Content { get; set; } = default!;
+    public DateTime OccurredOn { get; set; }
+    public DateTime? ProcessedOn { get; set; }
+}
